181235a024358128a12b233dcea2a4d60b440e34
JMJHOX <josemiguelaparicio507@gmail.com>
2020-08-27 17:45:25 -0500

    MCV-pt2

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -13,6 +13,23 @@ Bicicleta.allBicis=[];
 Bicicleta.add= function(aBici){
     Bicicleta.allBicis.push(aBici);
 }
+
+Bicicleta.findById =function(aBiciId){
+    var aBici = Bicicleta.allBicis.find(x=> x.id ==aBiciId);
+    if(aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicilcleta con el id ${aBiciId}`);
+}
+Bicicleta.removeById = function(aBiciId){
+for(var i=0; i < Bicicleta.allBicis.length; i++){
+    if (Bicicleta.allBicis[i].id ==aBiciId){
+        Bicicleta.allBicis.splice(i,1);
+    }
+}
+
+}
+
 var a= new Bicicleta(1,'rojo', 'urbana', [51.509865,-0.118092]);
 var b= new Bicicleta(2,'blanca', 'urbana', [-34.596932,-58.3808287]);
 Bicicleta.add(a);
--- routes/bicicletas.js
+++ routes/bicicletas.js
@@ -5,7 +5,9 @@ var BicicletaController = require('../controllers/bicicleta');
 
 
 Router.get('/',BicicletaController.bicicleta_list);
+Router.get('/:id/update',BicicletaController.bicicleta_update_get);
 Router.get('/create', BicicletaController.bicicleta_create_get);
 Router.post('/create', BicicletaController.bicicleta_create_post);
-
+Router.post('/:id/delete',BicicletaController.bicicleta_delete_post);
+Router.post('/:id/update',BicicletaController.bicicleta_update_post);
 module.exports= Router;
